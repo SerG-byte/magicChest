13f5209e97a69417df72b11874b313c50d3641bc
Vladimir Gorbanev <git-commit-address>
2021-09-12 20:24:11 +0700

    start commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <div class="content">
+        <div class="headerUp">
+            <div class="itemHeaderUp">
+                Иконки
+            </div>
+            <div class="itemHeaderUp mainIcone">
+                Magic Chest
+            </div>
+            <div class="itemHeaderUp singIn">
+                +8 800 555 35 35
+            </div>
+        </div>
+        <div class="headerDown">
+            <button type="submit" class="headerButton">Text</button>
+            <button type="submit" class="headerButton">Text</button>
+            <button type="submit" class="headerButton">Text</button>
+            <button type="submit" class="headerButton">Text</button>
+            <button type="submit" class="headerButton">Text</button>
+            <button type="submit" class="headerButton">Text</button>
+        </div>
+    </div>
+    
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <title>Magic-Chest</title>
+</head>
+<body>
+    <?php require "header.php "?>
+    <div style="margin-top: 50px;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, minus.
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet.
+    </div>
+    <?php require "footer.php "?>
+</body>
+</html>
