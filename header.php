9d90f27e8afd02fcfcd93abbf556e04b71e0afd7
Vladimir Gorbanev <git-commit-address>
2021-09-18 20:36:19 +0700

    add media

--- header.php
+++ header.php
@@ -1,14 +1,16 @@
 <header class="header">
     <div class="content">
         <div class="headerUp">
-            <div class="itemHeaderUp">
-                Иконки
+            <div class="itemHeaderUp itemBetween">
+                <input type="button" value="telegram">
+                <input type="button" value="instagram">
             </div>
-            <div class="itemHeaderUp mainIcone">
+            <div class="itemHeaderUp itemMain">
                 Magic Chest
             </div>
-            <div class="itemHeaderUp singIn">
-                +8 800 555 35 35
+            <div class="itemHeaderUp itemBetween">
+                <input class="singIN" type="button" value="Войти">
+                <div class="header_burger"><span></span></div>
             </div>
         </div>
         <div class="headerDown">
@@ -19,6 +21,17 @@
             <button type="submit" class="headerButton">Text</button>
             <button type="submit" class="headerButton">Text</button>
         </div>
+    </div>
+    <div class="burger_menu">
+        <ul class="header_list">
+            <li>Главная</li>
+            <li><a href="">dasda</a></li>
+            <li><a href="">dasda</a></li>
+            <li><a href="">dasda</a></li>
+            <li><a href="">dasda</a></li>
+            <li><a href="">dasda</a></li>
+            <li><a href="">dasda</a></li>
+        </ul>
     </div>
     
 </header>
--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/header.css">
+    
     <title>Magic-Chest</title>
 </head>
 <body>
     <?php require "header.php "?>
-    <div style="margin-top: 50px;">
+    <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
     </div>
     <div>
@@ -18,6 +19,28 @@
     <div>
         Lorem ipsum dolor sit amet.
     </div>
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, atque quasi possimus corrupti nulla reprehenderit officiis minima quos beatae. Debitis laborum quod dolorem nam optio, ipsa perferendis impedit beatae tempore doloremque ex error, saepe mollitia accusantium ut ea non voluptates, unde nihil minus inventore esse rerum ullam? Consequatur, eaque? Odio, quia totam tempore officia suscipit rerum consectetur aspernatur ex maiores fugiat facilis itaque deserunt deleniti molestiae assumenda nisi, mollitia vel nobis accusantium facere perspiciatis aliquam ipsa, laudantium vero! Quod, natus! Fugit aspernatur qui voluptatum nostrum debitis, hic velit, similique blanditiis minus nemo ad quisquam fuga ducimus esse et pariatur. Reiciendis.</div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam doloribus fuga quis nesciunt pariatur dolorem! At ipsa ab recusandae qui porro similique adipisci nihil sed. Aliquid nulla recusandae maxime veniam?
+    </div>
     <?php require "footer.php "?>
+
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <script src="js/burgerScript.js"></script>
 </body>
 </html>
