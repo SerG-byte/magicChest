67e0a69014d37402e995e0a79d7310ee6a2e34b4
Vladimir Gorbanev <git-commit-address>
2021-09-22 20:37:24 +0700

    fixed burger

--- header.php
+++ header.php
@@ -2,15 +2,29 @@
     <div class="content">
         <div class="headerUp">
             <div class="itemHeaderUp itemBetween">
-                <input type="button" value="telegram">
-                <input type="button" value="instagram">
+                <div style="float: left;">
+                    <form action="" target="_blank">
+                        <input type="button" value="telegram" class="linkButton">
+                    </form>
+                </div>
+                <div style="float: left;">
+                    <form action="https://www.instagram.com/magic_chest22/?utm_medium=copy_link" target="_blank">
+                        <input type="submit" value="" class="linkButton linkButtonInstagram">
+                    </form>
+                </div>
+                <div class="header_burger"><span></span></div>
             </div>
             <div class="itemHeaderUp itemMain">
-                Magic Chest
+                <div>
+                     Magic Chest
+                </div>
+                <div class="slogan">
+                     Лозунг
+                </div>
             </div>
             <div class="itemHeaderUp itemBetween">
-                <input class="singIN" type="button" value="Войти">
-                <div class="header_burger"><span></span></div>
+                <input class="singIN" type="button" value="">
+                <input class="basket" type="button" value="">
             </div>
         </div>
         <div class="headerDown">
@@ -23,14 +37,15 @@
         </div>
     </div>
     <div class="burger_menu">
+    <div class="burger_menu_exit"><span></span></div>
         <ul class="header_list">
             <li>Главная</li>
-            <li><a href="">dasda</a></li>
-            <li><a href="">dasda</a></li>
-            <li><a href="">dasda</a></li>
-            <li><a href="">dasda</a></li>
-            <li><a href="">dasda</a></li>
-            <li><a href="">dasda</a></li>
+            <li><a href="./index.php">dasda</a></li>
+            <li><a href="./index.php">dasda</a></li>
+            <li><a href="./index.php">dasda</a></li>
+            <li><a href="./index.php">dasda</a></li>
+            <li><a href="./index.php">dasda</a></li>
+            <li><a href="./index.php">dasda</a></li>
         </ul>
     </div>
     
--- js/burgerScript.js
+++ js/burgerScript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $('.header_burger').click(function(event){
-        $('.header_burger,.burger_menu').toggleClass('active');
+    $('.header_burger,.burger_menu_exit').click(function(event){
+        $('.header_burger,.burger_menu,.burger_menu_exit').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
