20b118dc6bca53213018fec7ec756a8f2562d98c
Vladimir Gorbanev <GS_post@mail.ru>
2021-09-26 21:11:15 +0700

    banner's swiper

--- index.php
+++ index.php
@@ -8,47 +8,52 @@
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/header_popup.css">
 
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/banner.css">
     
     <title>Magic-Chest</title>
 </head>
 <body>
     <?php require "header.php "?>
 
-    <div class="slider-conteiner">
-        <div class="slideNews fade">
-            <a href=""><img src="./news/oneNews.jpg" class="banner"></a>
-            <div class="textNews">Caption One</div>
-        </div>
-        <div class="slideNews fade">
-            <a href=""><img src="./news/twoNews.jpg" style="width: 100%;"></a>
-            <div class="textNews">Caption Two</div>
-        </div>
-        <div class="slideNews fade">
-            <a href=""><img src="./news/threeNews.jpg" style="width: 100%;"></a>
-            <div class="textNews">Caption Three</div>
-        </div>
+    <div id="main_container">
 
-        <a class="prev" onclick="plusSlide(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlide(1)">&#10095;</a>
-    </div>
-    <br>
-    <div style="border: solid 1px green;">
-        <div style="text-align: center;">
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
+        <!-- banner's block --> 
+        <div class="banner-content">
+            <div class="swiper-container">
+                <div class="banner-catolog">
+                    <div class="banner-slide">
+                        <a href="https://www.google.ru/">
+                            <picture>
+                                <source media="(max-width: 767px)" srcset="img/bners/mob_iphone.jpg">
+                                <img src="img/bners/big_iphone.jpg" alt="iphone banner">
+                            </picture>
+                        </a>
+                    </div>
+                    <!-- <div class="banner-slide"></div>
+                    <div class="banner-slide"></div>
+                    <div class="banner-slide"></div>
+                    <div class="banner-slide"></div> -->
+                </div>
+                <button class="swiper-button prev">&#10094;</button>
+                <button class="swiper-button next">&#10095;</button>
+            </div>
+            <ul class="swiper-pagination">
+                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
+                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
+                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
+                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
+                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
+            </ul>
         </div>
     </div>
-    
     <?php require "footer.php "?>
-
+    
     <script
         src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
         crossorigin="anonymous"></script>
     <script src="js/burgerScript.js"></script>
-    <script src="js/sliderNews.js"></script>
+    <!-- <script src="js/sliderNews.js"></script> -->
     <script src="js/popup.js"></script>
 </body>
 </html>
