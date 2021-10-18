fe728eccc340626be1cfd5fefbfc621e31d61231
Vladimir Gorbanev <GS_post@mail.ru>
2021-10-18 15:02:32 +0700

    Banner change

--- index.php
+++ index.php
@@ -22,28 +22,56 @@
         <div class="banner-content">
             <div class="swiper-container">
                 <div class="banner-catolog">
-                    <div class="banner-slide">
+                    <div class="banner-slide" style="display: none;">
                         <a href="https://www.google.ru/">
                             <picture>
-                                <source media="(max-width: 767px)" srcset="img/bners/mob_iphone.jpg">
-                                <img src="img/bners/big_iphone.jpg" alt="iphone banner">
+                                <source media="(max-width: 1022px)" srcset="img/bners/mob_iphone.jpg">
+                                <img src="img/bners/big_iphone.jpg" alt="iphone sales">
                             </picture>
                         </a>
                     </div>
-                    <!-- <div class="banner-slide"></div>
-                    <div class="banner-slide"></div>
-                    <div class="banner-slide"></div>
-                    <div class="banner-slide"></div> -->
+                    <div class="banner-slide" style="display: none;">
+                        <a href="https://www.google.ru/">
+                            <picture>
+                                <source media="(max-width: 1022px)" srcset="img\bners\mob_autumn.jpg">
+                                <img src="img\bners\big_autumn.jpg" alt="autumn sales">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="banner-slide" style="display: none;">
+                        <a href="https://www.google.ru/">
+                            <picture>
+                                <source media="(max-width: 1022px)" srcset="img\bners\mob_food.jpg">
+                                <img src="img\bners\big_food.jpg" alt="food sales">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="banner-slide" style="display: none;">
+                        <a href="https://www.google.ru/">
+                            <picture>
+                                <source media="(max-width: 1022px)" srcset="img\bners\mob_oral.jpg">
+                                <img src="img\bners\big_oral.jpg" alt="oral sales">
+                            </picture>
+                        </a>
+                    </div>
+                    <div class="banner-slide" style="display: none;">
+                        <a href="https://www.google.ru/">
+                            <picture>
+                                <source media="(max-width: 1022px)" srcset="img\bners\mob_philips.jpg">
+                                <img src="img\bners\big_philips.jpg" alt="philips sales">
+                            </picture>
+                        </a>
+                    </div>
                 </div>
                 <button class="swiper-button prev">&#10094;</button>
                 <button class="swiper-button next">&#10095;</button>
             </div>
             <ul class="swiper-pagination">
-                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
-                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
-                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
-                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
-                <li class="swiper-pagination-bullet" style="left: 0px;"></li>
+                <li class="swiper-pagination-bullet" style="left: 0px;">1</li>
+                <li class="swiper-pagination-bullet" style="left: 0px;">2</li>
+                <li class="swiper-pagination-bullet" style="left: 0px;">3</li>
+                <li class="swiper-pagination-bullet" style="left: 0px;">4</li>
+                <li class="swiper-pagination-bullet" style="left: 0px;">5</li>
             </ul>
         </div>
     </div>
