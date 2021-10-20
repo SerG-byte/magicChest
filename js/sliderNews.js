cbf2af17dce2e9924a73c5e16ed7dae150eb62cc
Vladimir Gorbanev <GS_post@mail.ru>
2021-10-20 15:24:51 +0700

    banner correction

--- js/sliderNews.js
+++ js/sliderNews.js
@@ -10,8 +10,8 @@ function currentSlide(n){
 }
 function showSlide(n){
     var i;
-    var slides = document.getElementsByClassName("slideNews");
-    var dots = document.getElementsByClassName("dot");
+    var slides = document.getElementsByClassName("banner-slide");
+    var dots = document.getElementsByClassName("swiper-pagination-bullet");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
@@ -25,12 +25,17 @@ function showSlide(n){
 }
 function showSlides(){
     var i;
-    var slides = document.getElementsByClassName("slideNews");
+    var slides = document.getElementsByClassName("banner-slide");
+    var dots = document.getElementsByClassName("swiper-pagination-bullet");
     for (i = 0; i < slides.length; i++){
         slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 10000);
 }
